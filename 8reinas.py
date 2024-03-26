0ffa03f4708b22dda35e9390a77e32851ea57794
OscarCarsi <oscarolivarezcarsi@gmail.com>
2024-03-26 15:54:51 -0600

    select 5 random boards

--- 8reinas.py
+++ 8reinas.py
@@ -1,6 +1,7 @@
+#Importa el modulo random para generar los aleatorios que se necesitaran
 import random as random;
 
-
+#se hace la generación de un tablero con las reinas en posiciones aleatorias
 def create_board(size_board):
     board = []
     for _ in range(size_board):
@@ -10,18 +11,24 @@ def create_board(size_board):
         board.append(row)
     return board
 
+#Se hace la generación de los tableros definidos, en esta caso 100 tableros, con el tamaño de 8x8
 def create_boards (size_board, num_boards):
     boards = []
     for _ in range(num_boards):
         board = create_board(size_board)
         boards.append(board)
     return boards
-
+#Se seleccionan 5 tableros aleatorios para iniciar la selección de los padres
+def select_random_boards(boards):
+    
+    return random.sample(boards, 5)
+    
 def main():
     size_board = 8
     num_boards = 100
     boards = create_boards(size_board, num_boards)
-    for i, board in enumerate(boards):
+    selectBoards = select_random_boards(boards)
+    for i, board in enumerate(selectBoards):
         print(f"Tablero {i+1}:")
         for row in board:
             print(' '.join(str(cell) for cell in row))
